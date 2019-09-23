2529f5a9136c57817fcd901b540f6fefc344625f
962n <tak.k1u2r2o4@gmail.com>
2019-09-23 17:27:24 +0900

    add docker for db

---
+++ docker/db/sql/001-create-tables.sql
@@ -0,0 +1,30 @@
+---- drop ----
+DROP TABLE IF EXISTS `todos`;
+
+DROP TABLE IF EXISTS `users`;
+
+---- create ----
+create table IF not exists `users`
+(
+ `id`               BIGINT UNSIGNED AUTO_INCREMENT,
+ `name`             VARCHAR(20) NOT NULL,
+ `email`            VARCHAR(100) NOT NULL,
+ `created_at`       TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ `updated_at`       TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+create table IF not exists `todos`
+(
+ `id`               INT UNSIGNED unsigned AUTO_INCREMENT,
+ `user_id`          BIGINT UNSIGNED,
+ `title`            VARCHAR(100),
+ `content`          TEXT,
+ `dead_line_at`     TIMESTAMP NOT NULL,
+ `completed_at`     TIMESTAMP NULL DEFAULT NULL,
+ `created_at`       TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ `updated_at`       TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY user_id_index(`user_id`) REFERENCES users(`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
